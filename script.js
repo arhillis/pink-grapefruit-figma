e892b8aa20e70d3ace6222a091e8a0b85ac8015d
Ann Hillis <andriellerhillis@gmail.com>
2019-12-02 14:56:32 -0600

    Finished the basic rotating feature

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 window.onload = () => {
-    console.log('Hello');
+    const steps = document.querySelectorAll('.step');
+
+    let btnIndex = 0;
+
+    const rotateSlides = () => {
+        steps[btnIndex].classList.remove('current');
+
+        btnIndex = btnIndex === steps.length - 1 ? 0 : btnIndex + 1;
+
+        steps[btnIndex].classList.add('current');
+
+    };
+
+    const rotation = setInterval(rotateSlides, 3000);
+
+    document.querySelector('#about').onclick = () => clearInterval(rotation);
 };
